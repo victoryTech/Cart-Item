c3b1b809bf4e3dd26abfd10d14f586ebdc80c669
unknown <Bieky Mahato>
2021-07-13 22:34:10 +0530

    Add increasing Quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,6 +9,15 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+        // biinding the multiple event handler in construcor
+        // bind is help to bind it to its refrernce
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+        // or we can use arrow function
+    }
+
+    // using arrow function to find the event lsitener function refremnce
+    increaseQuantity = () => {
+        console.log('this', this);
     }
     render () {
         // this is done coz we have to write this.state.title
@@ -27,9 +36,23 @@ class CartItem extends React.Component {
             {/* <div>Qty: 1</div> */}
             <div className="cart-item-actions">
               {/* Buttons */}
-              <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
-              <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1665/1665612.svg" />
-              <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+              <img 
+                alt="increase" 
+                className="action-icons" 
+                src="https://image.flaticon.com/icons/svg/992/992651.svg"
+                // onClick={this.increaseQuantity.bind(this)}
+                onClick={this.increaseQuantity}
+              />
+              <img 
+                alt="decrease" 
+                className="action-icons" 
+                src="https://image.flaticon.com/icons/svg/1665/1665612.svg" 
+              />
+              <img 
+                alt="delete" 
+                className="action-icons" 
+                src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+             />
             </div>
           </div>
         </div>
