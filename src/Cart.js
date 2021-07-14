43aecb63639d95251ed84bb69ca9b905b48f896d
unknown <Bieky Mahato>
2021-07-14 22:27:04 +0530

    Rendering list and mapping

--- src/Cart.js
+++ src/Cart.js
@@ -3,8 +3,19 @@ import CartItem from './CartItem';
 
 class Cart extends React.Component {
     render () {
+        const arr = [1, 2, 3, 4, 5];
         return (
             <div className="cart">
+                {/* // CartItem is componenet */}
+                {/* rendering all element of an array */}
+                {/* {arr} */}
+                {/* now increment all element by 5 */}
+                {/* by maping we can do this -> this will give new array adding 5 with every element */}
+                {/* thsi will give new array  */}
+                { arr.map( (item) => {
+                    return item + 5;
+                })
+                }
                 <CartItem />
                 <CartItem />
                 <CartItem />
