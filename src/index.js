b6395e280664cd54a3f6fb469e36469190c1c1d9
unknown <Bieky Mahato>
2021-07-16 00:44:04 +0530

    Added firebase to cart

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import * as serviceWorker from './serviceWorker';
+// import * as firebase from 'firebase';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-OAaopsGS97EYaL9CTUyrS-RIzY-e71g",
+  authDomain: "cart-fe070.firebaseapp.com",
+  projectId: "cart-fe070",
+  storageBucket: "cart-fe070.appspot.com",
+  messagingSenderId: "830163513351",
+  appId: "1:830163513351:web:81eb42692036d81b783aa2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +27,5 @@ ReactDOM.render(
 );
 
  
+// serviceWorker.unregister();
+// npm install react-service-worker
